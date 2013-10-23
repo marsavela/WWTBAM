2375c7ae717bc52a70238876f492709f963de452
Sergiu Daniel <marsavela@gmail.com>
2013-10-23 09:05:05 +0200

    Arreglada la lectura desde XML.

--- WWTBAM/src/main/java/es/serpat/wwtbam/Game.java
+++ WWTBAM/src/main/java/es/serpat/wwtbam/Game.java
@@ -21,7 +21,7 @@ public class Game extends FragmentActivity {
 
     //Constants for saved data.
     //private String SHARED_PREF_GAME = "isGameSaved";
-    private String SHARED_PREF_QUESTION_NUMBER = "questionNumber";
+    //private String SHARED_PREF_QUESTION_NUMBER = getString(R.string.SHARED_PREF_QUESTION_NUMBER);
     //private String SHARED_PREF_NAME_KEY = "playerName";
     private String SHARED_PREF_AUDIENCE = "isUsedAudienceJoker";
     private String SHARED_PREF_FIFTY_USED = "questionFiftyJokerWereUsed";
@@ -125,7 +125,7 @@ public class Game extends FragmentActivity {
     public void saveGameData() {
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(activity);
         SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putInt(SHARED_PREF_QUESTION_NUMBER, actualQuestion);
+        editor.putInt(activity.getResources().getString(R.string.SHARED_PREF_QUESTION_NUMBER), actualQuestion);
         editor.putString(activity.getResources().getString(R.string.SHARED_PREF_NAME_KEY), playerName);
         //editor.putString(SHARED_PREF_NAME_KEY, playerName);
         editor.putBoolean(SHARED_PREF_PHONE, isUsedPhoneJoker);
@@ -137,7 +137,7 @@ public class Game extends FragmentActivity {
 
     public void restoreGameData() {
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(activity);
-        actualQuestion = sharedPref.getInt(SHARED_PREF_QUESTION_NUMBER, 0);
+        actualQuestion = sharedPref.getInt(activity.getResources().getString(R.string.SHARED_PREF_QUESTION_NUMBER), 0);
         playerName = sharedPref.getString(activity.getResources().getString(R.string.SHARED_PREF_NAME_KEY),
                 activity.getResources().getString(R.string.default_user_name));
         //playerName = sharedPref.getString(SHARED_PREF_NAME_KEY, activity.getResources().getString(R.string.default_user_name));
@@ -191,236 +191,6 @@ public class Game extends FragmentActivity {
     public List<Question> generateQuestionList() throws XmlPullParserException {
         List<Question> list = new ArrayList<Question>();
         Question q;
-
-        q = new Question(
-                "1",
-                "Which is the Sunshine State of the US?",
-                "North Carolina",
-                "Florida",
-                "Texas",
-                "Arizona",
-                "2",
-                "2",
-                "2",
-                "1",
-                "4"
-        );
-        list.add(q);
-
-        q = new Question(
-                "2",
-                "Which of these is not a U.S. state?",
-                "New Hampshire",
-                "Washington",
-                "Wyoming",
-                "Manitoba",
-                "4",
-                "4",
-                "4",
-                "2",
-                "3"
-        );
-        list.add(q);
-
-        q = new Question(
-                "3",
-                "What is Book 3 in the Pokemon book series?",
-                "Charizard",
-                "Island of the Giant Pokemon",
-                "Attack of the Prehistoric Pokemon",
-                "I Choose You!",
-                "3",
-                "2",
-                "3",
-                "1",
-                "4"
-        );
-        list.add(q);
-
-        q = new Question(
-                "4",
-                "Who was forced to sign the Magna Carta?",
-                "King John",
-                "King Henry VIII",
-                "King Richard the Lion-Hearted",
-                "King George III",
-                "1",
-                "3",
-                "1",
-                "2",
-                "3"
-        );
-        list.add(q);
-
-        q = new Question(
-                "5",
-                "Which ship was sunk in 1912 on its first voyage, although people said it would never sink?",
-                "Monitor",
-                "Royal Caribean",
-                "Queen Elizabeth",
-                "Titanic",
-                "4",
-                "4",
-                "4",
-                "1",
-                "2"
-        );
-        list.add(q);
-
-        q = new Question(
-                "6",
-                "Who was the third James Bond actor in the MGM films? (Do not include Casino Royale.)",
-                "Roger Moore",
-                "Pierce Brosnan",
-                "Timothy Dalton",
-                "Sean Connery",
-                "1",
-                "3",
-                "3",
-                "2",
-                "3"
-        );
-        list.add(q);
-
-        q = new Question(
-                "7",
-                "Which is the largest toothed whale?",
-                "Humpback Whale",
-                "Blue Whale",
-                "Killer Whale",
-                "Sperm Whale",
-                "4",
-                "2",
-                "2",
-                "2",
-                "3"
-        );
-        list.add(q);
-
-        q = new Question(
-                "8",
-                "In what year was George Washington born?",
-                "1728",
-                "1732",
-                "1713",
-                "1776",
-                "2",
-                "2",
-                "2",
-                "1",
-                "4"
-        );
-        list.add(q);
-
-        q = new Question(
-                "9",
-                "Which of these rooms is in the second floor of the White House?",
-                "Red Room",
-                "China Room",
-                "State Dining Room",
-                "East Room",
-                "2",
-                "2",
-                "2",
-                "3",
-                "4"
-        );
-        list.add(q);
-
-        q = new Question(
-                "10",
-                "Which Pope began his reign in 963?",
-                "Innocent III",
-                "Leo VIII",
-                "Gregory VII",
-                "Gregory I",
-                "2",
-                "1",
-                "2",
-                "3",
-                "4"
-        );
-        list.add(q);
-
-        q = new Question(
-                "11",
-                "What is the second longest river in South America?",
-                "Parana River",
-                "Xingu River",
-                "Amazon River",
-                "Rio Orinoco",
-                "1",
-                "1",
-                "1",
-                "2",
-                "3"
-        );
-        list.add(q);
-
-        q = new Question(
-                "12",
-                "What Ford replaced the Model T?",
-                "Model U",
-                "Model A",
-                "Edsel",
-                "Mustang",
-                "2",
-                "4",
-                "4",
-                "1",
-                "3"
-        );
-        list.add(q);
-
-        q = new Question(
-                "13",
-                "When was the first picture taken?",
-                "1860",
-                "1793",
-                "1912",
-                "1826",
-                "4",
-                "4",
-                "4",
-                "1",
-                "3"
-        );
-        list.add(q);
-
-        q = new Question(
-                "14",
-                "Where were the first Winter Olympics held?",
-                "St. Moritz, Switzerland",
-                "Stockholm, Sweden",
-                "Oslo, Norway",
-                "Chamonix, France",
-                "4",
-                "1",
-                "4",
-                "2",
-                "3"
-        );
-        list.add(q);
-
-        q = new Question(
-                "15",
-                "Which of these is not the name of a New York tunnel?",
-                "Brooklyn-Battery",
-                "Lincoln",
-                "Queens Midtown",
-                "Manhattan",
-                "4",
-                "4",
-                "4",
-                "1",
-                "3"
-        );
-        list.add(q);
-
-        return list;
-
-        /*List<Question> list = new ArrayList<Question>();
-        Question q;
         InputStream inputStream = activity.getResources().openRawResource(R.raw.questions0001);
 
         XmlPullParser parser = XmlPullParserFactory.newInstance().newPullParser();
@@ -429,7 +199,7 @@ public class Game extends FragmentActivity {
         int eventType = XmlPullParser.START_DOCUMENT;
 
         while (eventType != XmlPullParser.END_DOCUMENT) {
-            if (eventType == XmlPullParser.START_TAG) {
+            if (eventType == XmlPullParser.START_TAG && parser.getName().equals("question")) {
                 q = new Question(
                         parser.getAttributeValue(null,"number"),
                         parser.getAttributeValue(null,"text"),
@@ -454,100 +224,6 @@ public class Game extends FragmentActivity {
             }
         }
 
-        try {
-            inputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return list;*/
-    }
-    public List<Question> generateQuestionList2() throws XmlPullParserException {
-
-        List<Question> list = new ArrayList<Question>();
-        Question q;
-        InputStream inputStream = activity.getResources().openRawResource(R.raw.questions0001);
-
-        XmlPullParser parser = XmlPullParserFactory.newInstance().newPullParser();
-        parser.setInput(inputStream, null);
-
-        int eventType = XmlPullParser.START_DOCUMENT;
-
-        while (eventType != XmlPullParser.END_DOCUMENT) {
-            q = new Question();
-            if (eventType == XmlPullParser.START_TAG) {
-                q.number = parser.getAttributeValue(null, "number");
-                q.text = parser.getAttributeValue(null,"text");
-                q.answer1 = parser.getAttributeValue(null,"answer1");
-                q.answer2 = parser.getAttributeValue(null,"answer2");
-                q.answer3 = parser.getAttributeValue(null,"answer3");
-                q.answer4 = parser.getAttributeValue(null,"answer4");
-                q.right = parser.getAttributeValue(null,"right");
-                q.audience = parser.getAttributeValue(null,"audience");
-                q.phone = parser.getAttributeValue(null,"phone");
-                q.fifty1 = parser.getAttributeValue(null,"fifty1");
-                q.fifty2 = parser.getAttributeValue(null,"fifty2");
-            }
-            list.add(q);
-            try {
-                eventType = parser.next();
-            } catch (XmlPullParserException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        try {
-            inputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return list;
-    }
-    public List<Question> generateQuestionList3() throws XmlPullParserException {
-
-        List<Question> list = new ArrayList<Question>();
-        Question q;
-        InputStream inputStream = activity.getResources().openRawResource(R.raw.questions0001);
-
-        XmlPullParser parser = null;
-        try {
-            parser = XmlPullParserFactory.newInstance().newPullParser();
-        } catch (XmlPullParserException e) {
-            e.printStackTrace();
-        }
-        try {
-            parser.setInput(inputStream, null);
-        } catch (XmlPullParserException e) {
-            e.printStackTrace();
-        }
-        int eventType = XmlPullParser.START_DOCUMENT;
-
-        while (eventType != XmlPullParser.END_DOCUMENT) {
-            if (eventType == XmlPullParser.START_TAG) {
-                q = new Question(
-                        parser.getAttributeValue(null, "number"),
-                        parser.getAttributeValue(null,"text"),
-                        parser.getAttributeValue(null,"answer1"),
-                        parser.getAttributeValue(null,"answer2"),
-                        parser.getAttributeValue(null,"answer3"),
-                        parser.getAttributeValue(null,"answer4"),
-                        parser.getAttributeValue(null,"right"),
-                        parser.getAttributeValue(null,"audience"),
-                        parser.getAttributeValue(null,"phone"),
-                        parser.getAttributeValue(null,"fifty1"),
-                        parser.getAttributeValue(null,"fifty2")
-                );
-                list.add(q);
-                Log.v("XML", parser.getAttributeValue(null, "number"));
-            }
-            try {
-                eventType = parser.next();
-            } catch (XmlPullParserException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
         try {
             inputStream.close();
         } catch (IOException e) {
