f5c10af2e5471b5b44d6dfdafa67c731e7fe2432
marsavela <marsavela@gmail.com>
2013-11-07 01:12:51 +0100

    Recuperando FriendsActivity.

--- WWTBAM/src/main/java/es/serpat/wwtbam/FriendsActivity.java
+++ WWTBAM/src/main/java/es/serpat/wwtbam/FriendsActivity.java
@@ -1,9 +1,6 @@
 package es.serpat.wwtbam;
 
-import android.app.ActionBar;
-import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.FragmentTransaction;
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -11,57 +8,27 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.StrictMode;
 import android.preference.PreferenceManager;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.ListFragment;
 import android.support.v4.app.NavUtils;
-import android.support.v4.view.ViewPager;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.Toast;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URLEncodedUtils;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicHttpResponse;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Created by SergiuDaniel on 13/10/13.
@@ -84,8 +51,6 @@ public class FriendsActivity extends ListActivity  {
                 android.R.layout.simple_list_item_1, values);
         setListAdapter(adapter);
 
-//        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitNetwork().build();
-//        StrictMode.setThreadPolicy(policy);
     }
 
     @Override
@@ -112,13 +77,11 @@ public class FriendsActivity extends ListActivity  {
         return super.onOptionsItemSelected(item);
     }
 
-
     private void add_new_friend() {
 
         name=PreferenceManager.getDefaultSharedPreferences(context).
                 getString(context.getResources().getString(R.string.SHARED_PREF_NAME_KEY),
                         context.getResources().getString(R.string.default_user_name));
-        Toast.makeText(context, name, Toast.LENGTH_LONG).show();
 
         AlertDialog.Builder alert = new AlertDialog.Builder(context);
         alert.setTitle(R.string.action_addFriend); //Set Alert dialog title here
@@ -136,10 +99,13 @@ public class FriendsActivity extends ListActivity  {
                 friend_name = input.getEditableText().toString();
                 Toast.makeText(context, friend_name, Toast.LENGTH_LONG).show();
 
-    //            post_friend();
-     //           miThread();
-                PostFriendTask task = new PostFriendTask();
-                task.execute();
+                if (isConnected()) {
+                    SendFriendTask task = new SendFriendTask();
+                    task.execute();
+                }
+                else {
+                    Toast.makeText(getParent(), getResources().getString(R.string.not_connected), Toast.LENGTH_SHORT).show();
+                }
 
             }
         }); //End of alert.setPositiveButton
@@ -154,112 +120,41 @@ public class FriendsActivity extends ListActivity  {
 
     }
 
-    private class PostFriendTask extends AsyncTask<Void, Integer, Boolean> {
-
-        @Override
-        protected void onPreExecute() {
-            // TODO Auto-generated method stub
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-        // TODO Auto-generated method stub
-
-
-                    if (isConnected()) {
-
-                        List<NameValuePair> pairs = new ArrayList<NameValuePair>();
-                        pairs.add(new BasicNameValuePair("name", name));
-                        pairs.add(new BasicNameValuePair("friend_name", friend_name));
-
-                        try {
-                            URL url = new URL("http://wwtbamandroid.appspot.com/rest/friends");
-                            HttpURLConnection connection =(HttpURLConnection) url.openConnection();
-                            connection.setRequestMethod("POST");
-                            connection.setDoOutput(true);
-                            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream(), "UTF-8"));
-                            writer.write(URLEncodedUtils.format(pairs, "UTF-8"));
-                            writer.close();
-
-                            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                        } catch (MalformedURLException e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
-                        } catch (IOException e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
-                        }
-                        //handle.post(proceso);
-                    }
-                    else {
-                        Toast.makeText(getParent(), getResources().getString(R.string.not_connected), Toast.LENGTH_SHORT).show();
-                    }
-
-            return true;
-        }
-
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-        // TODO Auto-generated method stub
-            // Change something in the interface
-        }
-    }
-
-    private class GetFriendsTask extends AsyncTask<Void, Integer, Boolean> {
-
-        @Override
-        protected void onPreExecute() {
-            // TODO Auto-generated method stub
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            // TODO Auto-generated method stub
-
-
-            if (isConnected()) {
-
-                List<NameValuePair> pairs = new ArrayList<NameValuePair>();
-                pairs.add(new BasicNameValuePair("name", name));
-                pairs.add(new BasicNameValuePair("friend_name", friend_name));
-
-                try {
-                    URL url = new URL("http://wwtbamandroid.appspot.com/rest/friends");
-                    HttpURLConnection connection =(HttpURLConnection) url.openConnection();
-                    connection.setRequestMethod("POST");
-                    connection.setDoOutput(true);
-                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream(), "UTF-8"));
-                    writer.write(URLEncodedUtils.format(pairs, "UTF-8"));
-                    writer.close();
-
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                } catch (MalformedURLException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-                //handle.post(proceso);
-            }
-            else {
-                Toast.makeText(getParent(), getResources().getString(R.string.not_connected), Toast.LENGTH_SHORT).show();
-            }
-
-            return true;
-        }
-
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-            // TODO Auto-generated method stub
-            // Change something in the interface
-        }
-    }
-
     public boolean isConnected() {
         ConnectivityManager manager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
         NetworkInfo info = manager.getActiveNetworkInfo();
         return ((info != null) && (info.isConnected()));
+    }
+
+    private class SendFriendTask extends AsyncTask<Void, Integer, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO Auto-generated method stub
+
+            List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+            pairs.add(new BasicNameValuePair("name", name));
+            pairs.add(new BasicNameValuePair("friend_name", friend_name));
+
+            try {
+                URL url = new URL("http://wwtbamandroid.appspot.com/rest/friends");
+                HttpURLConnection connection =(HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setDoOutput(true);
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream(), "UTF-8"));
+                writer.write(URLEncodedUtils.format(pairs, "UTF-8"));
+                writer.close();
+                connection.getInputStream();
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            return true;
+        }
     }
 
 }
